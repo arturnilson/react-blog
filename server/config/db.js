46e7824b3d306f59cc175596d92f51bc29b0d825
Artur Nilson <arturnilson@gmail.com>
2020-12-09 16:14:30 -0300

    Finish front and back-end, fixed problem with db connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: "localhost:3306",
-    user: "root",
-    password: "4l0n3f4st3r",
-    database: "blogposts"
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+    database: 'blogposts'
 });
 
 module.exports = db
--- server/index.js
+++ server/index.js
@@ -1,20 +1,54 @@
 const express = require('express');
 const db = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = 3001;
 
-app.get('/', (req, res) => { });
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/get', (req, res) => {
+    db.query("SELECT * FROM posts",
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+
+            res.send(result);
+        });
+});
+
+app.get('/api/getFromId/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.query("SELECT * FROM posts WHERE id = ?", id,
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+
+            res.send(result);
+        });
+});
 
 app.post("/api/create", (req, res) => {
-    db.query("INSERT INTO posts (title, post_text, username) VALUES ()", (err, result) => {
-        if (err) {
-            console.log(err);
-        }
+    const username = req.body.userName;
+    const title = req.body.title;
+    const text = req.body.text;
 
-        console.log(result);
-    });
+    console.log(username, title, text);
+
+    db.query("INSERT INTO posts (title, post_text, username) VALUES (?,?,?)",
+        [title, text, username],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+
+            console.log(result);
+        });
 });
 
 
